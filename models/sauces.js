12383bb51276d45b771859040359b9f215a057c5
fairou <94571146+fairou@users.noreply.github.com>
2022-05-06 15:41:22 +0200

    fix token add security update/delete sauces helmet dotenv secure header - crypto-js

--- models/sauces.js
+++ models/sauces.js
@@ -1,18 +1,21 @@
+//Importation de mongoose
 const mongoose = require('mongoose');
+
+//Creation du model pour les sauces
 const saucesSchema = mongoose.Schema({
-    //_id: { type: String, required: true },
+    userId: { type: String, required: true, },
     name: { type: String, required: true, },
     manufacturer: { type: String, required: true, },
     description: { type: String, required: true, },
+    mainPepper: { type: String, required: true, },
+    imageUrl: { type: String, required: true, },
     heat: { type: Number, required: true, },
     likes: { type: Number, default: 0, },
     dislikes: { type: Number, default: 0, },
-    imageUrl: { type: String, required: true, },
-    mainPepper: { type: String, required: true, },
     usersLiked: { type: [String], },
     usersDisliked: { type: [String], },
-    userId: { type: String, required: true, },
+
 
 });
-
+//Exportation du model
 module.exports = mongoose.model('sauces', saucesSchema);
